d870e5b842a15fc5b9334c9c77deb1c2ec77373d
AndreasHjorth11 <77662628+AndreasHjorth11@users.noreply.github.com>
2022-10-05 20:26:17 +0200

    Add Login Auth

--- src/App.js
+++ src/App.js
@@ -7,20 +7,23 @@ import Details from "./Pages/Details";
 import Details2 from "./Pages/Details2";
 import Login from "./Pages/Login";
 import Search from "./Pages/Search";
+import { AuthContextProvider } from "./Context/Authcontext";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route exact path="/" element={<Welcome />} />
-        <Route exact path="/aktiviteter" element={<Activities />} />
-        <Route exact path="/aktivitet/1/detaljer" element={<Details />} />
-        <Route exact path="/aktivitet/2/detaljer" element={<Details2 />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/search" element={<Search />} />
-      </Routes>
-      <Test />
-    </div>
+    <AuthContextProvider>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Welcome />} />
+          <Route exact path="/aktiviteter" element={<Activities />} />
+          <Route exact path="/aktivitet/1/detaljer" element={<Details />} />
+          <Route exact path="/aktivitet/2/detaljer" element={<Details2 />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/search" element={<Search />} />
+        </Routes>
+        <Test />
+      </div>
+    </AuthContextProvider>
   );
 }
 
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,13 +1,50 @@
+import { AuthContext } from "../Context/Authcontext";
+import { createRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useRef, useState, useEffect, useContext } from "react";
 import Hero from "../Components/Hero";
-import LoginButton from "../Components/LogIndButton";
+// import LoginButton from "../Components/LogIndButton";
 import "../index.css";
 
 const Login = () => {
+  let { setIsAuthticated } = useContext(AuthContext);
+  let navigate = useNavigate();
+  let username = createRef();
+  let password = createRef();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:4000/auth/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `username=${username.current.value}&password=${password.current.value}`,
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          setLoginError(true);
+          throw new Error("unauthorized");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setIsAuthticated(data.token);
+
+        navigate("/aktiviteter");
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const [loginError, setLoginError] = useState(undefined);
+
   return (
     <div className="relative overflow-hidden">
       <Hero image="splash-image.jpg" />
       <div>
-        <div className="absolute w-[756px] h-[479px] bg-JapaneseViolet opacity-30 rotate-[-27.19deg] top-0 mt-[130px] ml-[-160px] z-10" />
+        <div className="absolute w-[756px] h-[479px] bg-JapaneseViolet opacity-30 rotate-[-27.19deg] top-0 mt-[130px] ml-[-160px] z-10 animate-fade" />
       </div>
       <div className="w-full h-52  z-20 absolute mt-[-530px] flex">
         <div className="w-full absolute">
@@ -19,17 +56,23 @@ const Login = () => {
             type="text"
             className="h-14 pl-5 "
             id="inputID"
+            ref={username}
           ></input>
           <input
             placeholder="Adgangskode"
             type="password"
             className="h-14 pl-5"
             id="inputID"
+            ref={password}
           ></input>
         </div>
         <div className="flex w-full absolute justify-center mt-[180px]">
           <div className="absolute text-center">
-            <LoginButton />
+            <Link to="/aktiviteter">
+              <div className="bg-JapaneseViolet w-[250px] h-[54px] rounded-xl font-bold text-lg animate-fade drop-shadow-[0_9px_5px_rgba(0,0,0,0.35)]">
+                <button onClick={(e) => handleLogin(e)} className="mt-[11px] text-Plaster">Log Ind</button>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
